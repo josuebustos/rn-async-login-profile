1fb8d0eafff9d29945352532237b71c94d600c5f
Josue Bustos <829764+josuebustos@users.noreply.github.com>
2022-08-26 23:46:03 -0700

    working async storage update

--- components/Settings/Session.js
+++ components/Settings/Session.js
@@ -2,48 +2,43 @@ import React, { useState, useEffect } from "react"
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Text } from "native-base";
 
-
-
 const GetSession = () => {
 
   const [data, setData] = useState([]);
 
+  const showText = () => {    
+    if (data.length === 0) {
+      return <Text>No Data</Text>;
+    } else{      
+      return <Text>{data.company.description.blocks[0].text}</Text>;
+    }
+  }
+
   const start = async () => {
     try {
       const value = await AsyncStorage.getItem('@storage_Key')
-      if (value !== null) {                
+      if (value !== null) {
         const jsonObj = JSON.parse(value);
         setData(jsonObj);
-        console.log(data.first_name)
+        // console.log(data.first_name)
       } else {
-
+        console.log("no data found");
       }
     } catch (e) {
-      console.log(e);
+      setData(null);
+      console.log("error");
       // error reading value
     }
-
-
   }
+
   useEffect(() => {
     start();
   }, [])
-  
 
-  return <Text>{data.first_name}</Text>;
+
+  return <>{showText()}</>;
 
 }
-
-// const SessionStatus = async (value) => {
-//   try {
-//     const jsonValue = JSON.stringify(value)
-//     await AsyncStorage.setItem('@Session_Key', jsonValue)
-
-//   } catch (e) {
-//     console.log(e)
-//     // saving error
-//   }
-// }
 
 export {
   GetSession
